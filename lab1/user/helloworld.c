02c63609c5a5596213087d25c7012d500ade7abf
zufehezhu <1981973792@qq.com>
2023-12-13 09:58:24 +0800

    lab2差功能实现

--- lab1/user/helloworld.c
+++ lab1/user/helloworld.c
@@ -1,7 +1,8 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-int main(){
+int main(int argc , char *argv[]){
+    printf("Hello %s!\n", argv[0]);
     printf("Hello World!\n");
     exit(0);
 }
