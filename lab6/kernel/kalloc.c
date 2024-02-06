e9333c65c47434a58a758acbfbfbc2a59581a0c6
zufehezhu <1981973792@qq.com>
2024-02-06 14:56:35 +0800

    删除.git lab3 和 lab8的buffer cache 优化没做

---
+++ lab6/kernel/kalloc.c
@@ -0,0 +1,103 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+int cow_cnt[PHYSTOP/PGSIZE];
+int get_num(uint64 pa){
+  int res = pa / PGSIZE;
+  return res;
+}
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    //先要知1 kfree 中再健一
+    cow_cnt[get_num((uint64)p)] = 1;
+    kfree(p);
+  }
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  acquire(&kmem.lock);
+  int rem = --cow_cnt[get_num((uint64)pa)];
+  release(&kmem.lock);
+  if(rem > 0)return;
+
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  int idx = get_num((uint64)r);
+  if(cow_cnt[idx] != 0)printf("%d , new page is not empty\n" , cow_cnt[idx]);
+  cow_cnt[idx] = 1;
+  return (void*)r;
+}
+
+void helper(uint64 pa , int num){
+  if(pa >= PHYSTOP)panic("物理地址不在这");
+  acquire(&kmem.lock);
+  cow_cnt[get_num(pa)] += num;
+  release(&kmem.lock);
+}
